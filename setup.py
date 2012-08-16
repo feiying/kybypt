2250b0967e9a9f8f07a953a2e2f28b8baeb0fd71
xiaoying.hu <xiaoying.hu@cs2c.com.cn>
2012-08-16 13:44:29 +0800

    update

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ from distutils.core import setup
 setup(name='kybypt',
 		version='1.0',
 		py_Modules=['kybypt'],
+		#packages=['kybypt'],
+		package_dir={'*.py':'*.py'},
+		#glob.glob(os.path.join('*.py')),
+        #os.path.listdir(os.path.join('*.py'))
 		)

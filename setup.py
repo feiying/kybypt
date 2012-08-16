6c049e4e22d16cc2764294a0e47fbe6a163ec775
xiaoying.hu <xiaoying.hu@cs2c.com.cn>
2012-08-16 14:08:33 +0800

    update

--- setup.py
+++ setup.py
@@ -4,9 +4,15 @@ from distutils.core import setup
 
 setup(name='kybypt',
 		version='1.0',
+		author='xiaoying.hu',
+		author_email='xiaoying.hu@cs2c.com.cn',
+		url='https://github.com/feiying/kybypt',
+		description='fetch update code from github, generate source rpm, and put srpm into koji to compile.',
+		license='LGPLv2',
 		py_Modules=['kybypt'],
 		#packages=['kybypt'],
-		package_dir={'*.py':'*.py'},
+		package_dir={'checking-github-update':'src/checking-github-update.py'},
+		data_files=[('/usr/share/kybypt/doc', ['docs/Generating-SSH-Keys_github-help.pdf'])]
 		#glob.glob(os.path.join('*.py')),
         #os.path.listdir(os.path.join('*.py'))
 		)
